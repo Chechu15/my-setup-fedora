9e3d4b3f55ec8e7a2e5a460a15a5efaa9458a8b4
Chechu15 <github777.aviator716@slmails.com>
2026-06-03 18:28:24 +0200

    Cambios en los comentarios

--- instalacion_programas.sh
+++ instalacion_programas.sh
@@ -98,7 +98,7 @@ wget https://extensions.libreoffice.org/assets/downloads/508/1735925190/codehigh
 
 echo "Paso 9: Descarga de codecs propietarios en rpm-fusion"
 
-### instalaacion de codecs faltantes rpm-fusion
+### instalación de codecs faltantes rpm-fusion
 
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 
@@ -121,4 +121,6 @@ echo "Paso 11: Limpieza de paquetes"
 
 sudo dnf autoremove -y
 
+
+echo "Final del Script!!!!!!"
 
